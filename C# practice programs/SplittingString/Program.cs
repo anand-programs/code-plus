d9be1a7611ba9ffabf8526a6fbe83153219fabe4
anand91bitbucket <dhanabal@i-t-plus.co.jp>
2020-04-17 10:42:28 +0900

    SplittingStringのプログラムを追加

---
+++ C# practice programs/SplittingString/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SplittingString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter names of players separated by comma : ");
+            string players = Console.ReadLine();
+
+            Console.WriteLine(players);
+
+            string[] playerName = players.Split(',');
+
+            foreach (string str in playerName)
+            {
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
