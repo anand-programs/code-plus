e4fb43fd2af36237f0055b4b780ae8d328f1f22d
anand91bitbucket <dhanabal@i-t-plus.co.jp>
2020-04-17 10:18:09 +0900

    HelloWorldを追加

---
+++ C# practice programs/NewCSharpPrograms/HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C_sharp_programs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
