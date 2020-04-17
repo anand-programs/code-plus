0987b3741440d05b6dcc64e2770db67814882c65
anand91bitbucket <dhanabal@i-t-plus.co.jp>
2020-04-17 10:38:01 +0900

    RandomNumbersのプログラムを追加

---
+++ C# practice programs/NewCSharpCodes/RandomNumbers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RandomNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int number = random.Next(1, 20);
+            Console.WriteLine("There are " + number + " number of apples");
+        }
+    }
+}
